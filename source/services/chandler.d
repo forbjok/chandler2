9a9ac2b66e2441d63ac0321384c5da96e0a6b04b
Forb.Jok <forbjok@gmail.com>
2016-01-27 01:59:29 +0100

    feat: Basic working save and load of project

--- source/services/chandler.d
+++ source/services/chandler.d
@@ -29,9 +29,9 @@ class ChandlerThread {
         return this._linkFilter.extensions;
     }
 
-    this(in string url, in string path) {
-        this._url = url;
-        this._path = path;
+    this(in char[] url, in char[] path) {
+        this._url = url.to!string;
+        this._path = path.to!string;
 
         this._parser = new FourChanThreadParser();
         this._linkFilter = new LinkFilter();
@@ -90,12 +90,37 @@ struct ThreadConfig {
 }
 
 ChandlerThread loadChandlerProject(in char[] path) {
-    return new ChandlerThread("","");
+    import std.file;
+    import stdx.data.json;
+    import jsonserialized;
+    import std.path;
+
+    auto projectDir = buildPath(path, ".chandler");
+    auto threadJsonPath = buildPath(projectDir, "thread.json");
+
+    // If project dir does not exist, create it
+    if (!projectDir.exists())
+        throw new Exception("Chandler project not found in: " ~ path.to!string);
+
+    // Read JSON configuration file
+    auto threadJson = readText(threadJsonPath);
+    auto jsonConfig = threadJson.toJSONValue();
+
+    ThreadConfig threadConfig;
+    threadConfig.deserializeFromJSONValue(jsonConfig);
+
+    auto chandler = new ChandlerThread(threadConfig.url, path);
+    foreach(ext; threadConfig.downloadExtensions) {
+        chandler.includeExtension(ext);
+    }
+
+    return chandler;
 }
 
 void saveProject(ChandlerThread chandlerThread) {
     import std.file;
     import stdx.data.json;
+    import jsonserialized;
     import std.path;
 
     auto downloadDir = chandlerThread.path;
@@ -111,4 +136,10 @@ void saveProject(ChandlerThread chandlerThread) {
     // If project dir does not exist, create it
     if (!projectDir.exists())
         mkdirRecurse(projectDir);
+
+    // Serialize configuration to JSON
+    auto jsonConfig = threadConfig.serializeToJSONValue();
+
+    // Write thread JSON to file
+    write(threadJsonPath, cast(void[])jsonConfig.toJSON());
 }
