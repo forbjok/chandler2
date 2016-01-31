72c481906deab29cb8e1a1cd822eabf5b0ae1d0a
Forb.Jok <forbjok@gmail.com>
2016-01-31 22:38:04 +0100

    wip: Download progress indicator

--- source/app.d
+++ source/app.d
@@ -2,7 +2,9 @@ import std.algorithm;
 import std.array;
 import std.conv;
 import std.file;
+import std.format;
 import std.path;
+import std.range;
 import std.stdio;
 import std.net.curl;
 
@@ -19,9 +21,35 @@ void main(string[] args)
 
     handleBreak();
 
-    //auto chandl = ChandlerProject.create(basePath, baseURL);
-    //chandl.save();
-    auto chandl = ChandlerProject.load(basePath);
+    auto chandl = ChandlerProject.create(basePath, baseURL);
+    chandl.save();
+    //auto chandl = ChandlerProject.load(basePath);
+
+    chandl.downloadLink = (url, destinationPath) {
+        import std.file;
+        import std.net.curl;
+        import std.path;
+
+        // Ensure that destination directory exists
+        auto destinationDir = destinationPath.dirName();
+        mkdirRecurse(destinationDir);
+
+        // If an exception is thrown during download, delete the broken file
+        scope(failure) std.file.remove(destinationPath);
+
+        auto file = File(destinationPath, "w");
+        scope(exit) file.close();
+
+        size_t downloadedBytes;
+        writefln("Downloading %s... ", url);
+        // Download file
+        foreach(chunk; std.net.curl.byChunk(url)) {
+            downloadedBytes += chunk.length;
+            string progr = "%d%%".format(downloadedBytes);
+            stdout.writef("%s%s", '\b'.repeat(progr.length), progr);
+            file.rawWrite(chunk);
+        }
+    };
 
     // Print info when a thread update occurred
     chandl.threadUpdated = (updateResult) {
@@ -38,6 +66,6 @@ void main(string[] args)
         writefln("Failed to download file: [%s]: %s.", url, message);
     };
 
-    //chandl.download();
-    chandl.rebuild();
+    chandl.download();
+    //chandl.rebuild();
 }
