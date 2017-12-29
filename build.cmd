243daef489f9d783dde2fe17a4ce35decee054ea
Forb.Jok <forbjok@gmail.com>
2017-12-29 17:12:41 +0100

    chore: Update Windows build script to use LDC2

--- build.cmd
+++ build.cmd
@@ -2,6 +2,6 @@
 
 set /p VERSION=< VERSION
 
-dub build -b release
+dub build --compiler=ldc2 --arch=x86 -b release
 7z a -mx=9 chandler_v%VERSION%.zip chandler.exe LICENSE.md README.md
 choco pack "chocolatey\package.nuspec"
